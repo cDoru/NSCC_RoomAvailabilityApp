9b757c68876e0f9721ba98b4dfe1e73842427268
Nick <W0290474@nscc.ca>
2017-03-06 13:34:46 -0400

    Got rid of login and register links

--- NsccRoomApp/NsccRoomApp_res/resources/views/layouts/app.blade.php
+++ NsccRoomApp/NsccRoomApp_res/resources/views/layouts/app.blade.php
@@ -67,23 +67,23 @@
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->first_name }}  {{ Auth::user()->last_name }}<span class="caret"></span>
-                        </a>
+                {{--@if (Auth::guest())--}}
+                    {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
+                    {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
+                {{--@else--}}
+                    {{--<li class="dropdown">--}}
+                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+                            {{--{{ Auth::user()->first_name }}  {{ Auth::user()->last_name }}<span class="caret"></span>--}}
+                        {{--</a>--}}
 
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                        </ul>
-                    </li>
+                        {{--<ul class="dropdown-menu" role="menu">--}}
+                            {{--<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
 
-                    <li><a href="{{ url('/Favourites') }}">Favourites</a></li>
+                    {{--<li><a href="{{ url('/Favourites') }}">Favourites</a></li>--}}
 
-                @endif
+                {{--@endif--}}
             </ul>
         </div>
     </div>
