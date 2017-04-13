8f7fb5ef6f5656b4178c21b9a7010afde5589052
ryansutc <ryansutc@gmail.com>
2017-04-13 18:26:34 -0300

    Updated version of mapbox api (js reference) this page uses.

--- NsccRoomApp/NsccRoomApp_res/resources/views/Locate.blade.php
+++ NsccRoomApp/NsccRoomApp_res/resources/views/Locate.blade.php
@@ -88,8 +88,8 @@
         {{--THIS SHOULD BE SEPERATE BLADE COMPONENT --}}
         <div id="roomstable">
         </div>
-        <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.32.1/mapbox-gl.js'></script>
-        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.32.1/mapbox-gl.css' rel='stylesheet' />
+        <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.35.1/mapbox-gl.js'></script>
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.35.1/mapbox-gl.css' rel='stylesheet' />
         <script src="{{ asset('js/appUI.js') }}"></script>
         <script src="{{ asset('js/mapEngine.js') }}"></script>
 
